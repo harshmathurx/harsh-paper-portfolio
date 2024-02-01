9a8a59963c71801c55ab33e3bbfee67a9e84bffe
Harsh Mathur <harshmathurx@gmail.com>
2024-02-01 13:38:39 +0530

    enabled vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
       <Navbar />
       <div className='px-6'>
         <Component {...pageProps} />
+        <Analytics />
       </div>
     </ThemeProvider>
   )
